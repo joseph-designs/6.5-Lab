941bb4e9c93c6c6aa2fb8f033f4755719a5ff307
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 16:08:29 +0000

    JS up

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ window. onload = function() {
             top: 0,
             right: 0,
             bottom: 0,
-            left: 100,}} );
+            left: 1000,}} );
     ScrollReveal().reveal('.Image3', {delay: 400, opacity: 0, rotate: {
         x: 0,
         y: 90,
         z: 0,}, 
         viewOffset: {
             top: 0,
-            right: 100,
+            right: 1000,
             bottom: 0,
             left: 0,}} );
     ScrollReveal().reveal('.Image4', {delay: 400, interval: 5, rotate: {
@@ -27,7 +27,7 @@ window. onload = function() {
             top: 0,
             right: 0,
             bottom: 0,
-            left: 100,}} );
+            left: 1000,}} );
     ScrollReveal().reveal('.Image5', {delay: 400, easing: 'cubic-bezier(0.5, 0, 0, 1)'});
 
 }
