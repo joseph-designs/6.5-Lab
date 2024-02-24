4d0d70a5e8e609cced439ae3a7f617977b061e12
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 00:38:35 +0000

    JS up

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ window. onload = function() {
     ScrollReveal().reveal('.Image2', {rotate: {
         x: 90,
         y: 0,
-        z: 0,});
+        z: 0,}});
     ScrollReveal().reveal('.Image3', {rotate: {
         x: 0,
         y: 90,
-        z: 0,});
+        z: 0,}});
     ScrollReveal().reveal('.Image4', {rotate: {
         x: 0,
         y: 0,
-        z: 90,});
+        z: 90,}});
     ScrollReveal().reveal('.Image5', {delay: 1000});
 
 }
