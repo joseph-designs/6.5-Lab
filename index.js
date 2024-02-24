7dcc4a26f03252a4452f265682b6b5dac51518d3
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 00:53:36 +0000

    JS up

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Add JavaScript below
 window. onload = function() {
-    ScrollReveal().reveal('.Image1', {delay: 1000});
-    ScrollReveal().reveal('.Image2', {delay: 1000, rotate: {
+    ScrollReveal().reveal('.Image1', {delay: 800});
+    ScrollReveal().reveal('.Image2', {delay: 800, rotate: {
         x: 90,
         y: 0,
         z: 0,}});
-    ScrollReveal().reveal('.Image3', {rotate: {
+    ScrollReveal().reveal('.Image3', {delay: 800, rotate: {
         x: 0,
         y: 90,
         z: 0,}});
-    ScrollReveal().reveal('.Image4', {rotate: {
+    ScrollReveal().reveal('.Image4', {delay: 800, rotate: {
         x: 0,
         y: 0,
         z: 90,}});
-    ScrollReveal().reveal('.Image5', {delay: 1000});
+    ScrollReveal().reveal('.Image5', {delay: 800});
 
 }
