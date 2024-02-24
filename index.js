0a44f2bb73c77455d68b7cef46de37bfcb4c217b
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 16:07:33 +0000

    JS up

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ window. onload = function() {
     ScrollReveal().reveal('.Image2', {delay: 400, origin: 'bottom', rotate: {
         x: 90,
         y: 0,
-        z: 0,}}, 
+        z: 0,}, 
         viewOffset: {
             top: 0,
             right: 0,
             bottom: 0,
-            left: 100,} ););
+            left: 100,}} );
     ScrollReveal().reveal('.Image3', {delay: 400, opacity: 0, rotate: {
         x: 0,
         y: 90,
-        z: 0,}}, 
+        z: 0,}, 
         viewOffset: {
             top: 0,
             right: 100,
             bottom: 0,
-            left: 0,} ););
+            left: 0,}} );
     ScrollReveal().reveal('.Image4', {delay: 400, interval: 5, rotate: {
         x: 0,
         y: 0,
-        z: 90,}}, 
+        z: 90,}, 
         viewOffset: {
             top: 0,
             right: 0,
             bottom: 0,
-            left: 100,} );
+            left: 100,}} );
     ScrollReveal().reveal('.Image5', {delay: 400, easing: 'cubic-bezier(0.5, 0, 0, 1)'});
 
 }
