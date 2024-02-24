979f6d214d06af5d1a8d9d785cf6e43fe9ba0e6a
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 16:05:09 +0000

    JS up

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // Add JavaScript below
 window. onload = function() {
-    ScrollReveal().reveal('.Image1', {delay: 800});
-    ScrollReveal().reveal('.Image2', {delay: 800, origin: 'bottom', rotate: {
+    ScrollReveal().reveal('.Image1', {delay: 400});
+    ScrollReveal().reveal('.Image2', {delay: 400, origin: 'bottom', rotate: {
         x: 90,
         y: 0,
-        z: 0,}});
-    ScrollReveal().reveal('.Image3', {delay: 800, opacity: 0, rotate: {
+        z: 0,}}, 
+        viewOffset: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 100,} ););
+    ScrollReveal().reveal('.Image3', {delay: 400, opacity: 0, rotate: {
         x: 0,
         y: 90,
-        z: 0,}});
-    ScrollReveal().reveal('.Image4', {delay: 800, interval: 5, rotate: {
+        z: 0,}}, 
+        viewOffset: {
+            top: 0,
+            right: 100,
+            bottom: 0,
+            left: 0,} ););
+    ScrollReveal().reveal('.Image4', {delay: 400, interval: 5, rotate: {
         x: 0,
         y: 0,
-        z: 90,}});
-    ScrollReveal().reveal('.Image5', {delay: 800, easing: 'cubic-bezier(0.5, 0, 0, 1)'});
+        z: 90,}}, 
+        viewOffset: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 100,} );
+    ScrollReveal().reveal('.Image5', {delay: 400, easing: 'cubic-bezier(0.5, 0, 0, 1)'});
 
 }
