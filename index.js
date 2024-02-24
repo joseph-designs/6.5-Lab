f4b3eabd46746ee042806b4e0c7b05bf7768ab89
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 17:18:20 +0000

    JS

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 // Add JavaScript below
 window. onload = function() {
-    ScrollReveal().reveal('.Image1', {delay: 400, reset: true,});
-    ScrollReveal().reveal('.Image2', {delay: 400, origin: 'bottom', rotate: {
+    ScrollReveal().reveal('.Image1', {delay: 400, 
+        reset: true,});
+    ScrollReveal().reveal('.Image2', {delay: 400, 
+        origin: 'bottom', 
+        rotate: {
         x: 90,
         y: 0,
         z: 0,}, 
-        scale: 2, origin: 'right', reset: true,} );
-    ScrollReveal().reveal('.Image3', {delay: 400, opacity: 0, rotate: {
+        scale: 2, 
+        origin: 'right', 
+        reset: true,} );
+    ScrollReveal().reveal('.Image3', {delay: 400, 
+        opacity: 0, 
+        rotate: {
         x: 0,
         y: 90,
         z: 0,}, 
-        scale: 2,reset: true,} );
-    ScrollReveal().reveal('.Image4', {delay: 400, interval: 5, rotate: {
+        scale: 2, 
+        reset: true,} );
+    ScrollReveal().reveal('.Image4', {delay: 400, 
+        interval: 5, 
+        rotate: {
         x: 0,
         y: 0,
-        z: 90,}
-        ,reset: true,
+        z: 90,},
+        reset: true,
         useDelay: 'always',} );
-    ScrollReveal().reveal('.Image5', {delay: 400, opacity: 10, reset: true,});
+    ScrollReveal().reveal('.Image5', {delay: 400, 
+        opacity: 10, 
+        reset: true,} );
 
 }
