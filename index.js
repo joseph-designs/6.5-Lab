38d3a3cbba72e1b62bb215f84e9740517c01ec32
joseph-designs <156028170+joseph-designs@users.noreply.github.com>
2024-02-24 00:43:46 +0000

    JS Up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add JavaScript below
 window. onload = function() {
     ScrollReveal().reveal('.Image1', {delay: 1000});
-    ScrollReveal().reveal('.Image2', {rotate: {
+    ScrollReveal().reveal('.Image2', {delay: 1000}, {rotate: {
         x: 90,
         y: 0,
         z: 0,}});
